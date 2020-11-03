68b5be20d3a012c36a81a28058d903a4e1e913b4
岡田清志郎 <>
2020-11-03 14:47:50 +0900

    コードを作成

---
+++ read_and_write.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <string.h>      //文字列の代入に使う
+#include <bits/stdc++.h> //piの利用で必要(M_PI)

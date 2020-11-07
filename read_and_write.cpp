82f244333bfae10a06843387614593fe5964ed0c
岡田清志郎 <>
2020-11-07 21:50:15 +0900

    ボンド結合の読み込みに成功

--- read_and_write.cpp
+++ read_and_write.cpp
@@ -25,9 +25,11 @@ public:
     /*結合*/
     uint32_t bond_pair[2];
     uint32_t bond_type[2];
+    uint32_t nbond = 0;
     /*アングル*/
     uint32_t angle_pair[2][3];
     uint32_t angle_type[2];
+    uint32_t nangle = 0;
 };
 
 int main(int argc, char *argv[])
@@ -109,36 +111,75 @@ int main(int argc, char *argv[])
         std::exit(EXIT_FAILURE);
     }
     //ファイルの読み込み
-    while (ifs1 >> temp_info.id >>  temp_info.velx >> temp_info.vely >> temp_info.velz)
+    while (ifs1 >> temp_info.id >> temp_info.velx >> temp_info.vely >> temp_info.velz)
     {
-        PI.at(temp_info.id-1).velx=temp_info.velx;
-        PI.at(temp_info.id-1).vely=temp_info.vely;
-        PI.at(temp_info.id-1).velz=temp_info.velz;
+        temp_info.id--;
+        PI.at(temp_info.id).velx = temp_info.velx;
+        PI.at(temp_info.id).vely = temp_info.vely;
+        PI.at(temp_info.id).velz = temp_info.velz;
     }
     ifs1.close();
-    for(int i = 0;i<10;i++){
-        std::cout<<PI.at(i).velx<<std::endl;
-    }
     /*
     
     
     
     
-    速度の読み込みを行う．*/
-    /*
-    std::ifstream ifs2("../input/" + argv[3]);
+    ボンド情報の読み込みを行う．*/
+    std::ifstream ifs2(argv[3]);
     if (!ifs2)
     {
         std::cerr << "error2" << std::endl;
         std::exit(EXIT_FAILURE);
     }
+    //ファイルの読み込み
+    while (ifs2 >> temp_info.bond_pair[0] >> temp_info.bond_pair[1] >> temp_info.bond_type[0])
+    {
+        int i = temp_info.bond_pair[0] - 1;
+        int j = temp_info.bond_pair[1] - 1;
+        PI.at(i).bond_pair[PI.at(i).nbond] = j;
+        PI.at(i).bond_type[PI.at(i).nbond] = temp_info.bond_type[0];
+        PI.at(i).nbond++;
+
+        PI.at(j).bond_pair[PI.at(j).nbond] = i;
+        PI.at(j).bond_type[PI.at(j).nbond] = temp_info.bond_type[0];
+        PI.at(j).nbond++;
+    }
+    ifs1.close();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << PI.at(i).bond_pair[0] + 1 << " " << PI.at(i).bond_pair[1] + 1 << std::endl;
+    }
+
+    /*
+    
+    
+    
+    
+    アングル情報の読み込みを行う．*/
 
     std::ifstream ifs3("../input/" + argv[4]);
     if (!ifs3)
     {
         std::cerr << "error3" << std::endl;
         std::exit(EXIT_FAILURE);
-    }*/
+    }
+    //ファイルの読み込み
+    while (ifs2 >> temp_info.bond_pair[0] >> temp_info.bond_pair[1] >> temp_info.bond_type[0])
+    {
+        int i = temp_info.bond_pair[0] - 1;
+        int j = temp_info.bond_pair[1] - 1;
+        PI.at(i).bond_pair[PI.at(i).nbond] = j;
+        PI.at(i).bond_type[PI.at(i).nbond] = temp_info.bond_type[0];
+        PI.at(i).nbond++;
 
+        PI.at(j).bond_pair[PI.at(j).nbond] = i;
+        PI.at(j).bond_type[PI.at(j).nbond] = temp_info.bond_type[0];
+        PI.at(j).nbond++;
+    }
+    ifs1.close();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << PI.at(i).bond_pair[0] + 1 << " " << PI.at(i).bond_pair[1] + 1 << std::endl;
+    }
     return 0;
 }

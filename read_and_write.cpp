2ec71a2e1529fb28b998bd99b475da2158d6edb6
岡田清志郎 <>
2020-11-09 13:28:51 +0900

    座標ファイルの出力に成功 なぜかはじめの3行だけ”diff”コマンドに引っかかる． ”PI”を”pinfo”に変更

--- read_and_write.cpp
+++ read_and_write.cpp
@@ -34,45 +34,7 @@ public:
 
 int main(int argc, char *argv[])
 {
-
-    //open file
-    //pos_file
-    FILE *fpo0;
-    fpo0 = fopen("../output/initial_pos_lipid.cdv", "a");
-    if (fpo0 == NULL)
-    {
-        printf("ERROR_initial_pos_lipid.cdv\n");
-        return -1;
-    }
-
-    //vel_file
-    FILE *fpo1;
-    fpo1 = fopen("../output/initial_vel_lipid.cdv", "a");
-    if (fpo1 == NULL)
-    {
-        printf("ERROR_initial_vel_lipid.cdv\n");
-        return -1;
-    }
-
-    //bond_file
-    FILE *fpo2;
-    fpo2 = fopen("../output/bond_info.cdv", "a");
-    if (fpo2 == NULL)
-    {
-        printf("ERROR_bond_info.cdv\n");
-        return -1;
-    }
-
-    //angle_file
-    FILE *fpo3;
-    fpo3 = fopen("../output/angle_info.cdv", "a");
-    if (fpo3 == NULL)
-    {
-        printf("ERROR_angle_info.cdv\n");
-        return -1;
-    }
-
-    std::vector<ParticleInfo> PI;
+    std::vector<ParticleInfo> pinfo;
     ParticleInfo temp_info;
     /*
     
@@ -87,16 +49,17 @@ int main(int argc, char *argv[])
         std::exit(EXIT_FAILURE);
     }
     //いらないはじめの3行を捨てる．
+    std::string delete_str[3];
     for (uint32_t i = 0; i < 3; i++)
     {
-        std::string delete_str;
-        std::getline(ifs0, delete_str);
+        std::getline(ifs0, delete_str[i]);
     }
-    //ファイルの読み込み　粒子番号は0から開始する．
+    //ファイルの読み込み　粒子番号,粒子種は0から開始する．
     while (ifs0 >> temp_info.id >> temp_info.type >> temp_info.posx >> temp_info.posy >> temp_info.posz)
     {
         temp_info.id--;
-        PI.push_back(temp_info);
+        temp_info.type--;
+        pinfo.push_back(temp_info);
     }
     ifs0.close();
     /*
@@ -115,9 +78,9 @@ int main(int argc, char *argv[])
     while (ifs1 >> temp_info.id >> temp_info.velx >> temp_info.vely >> temp_info.velz)
     {
         temp_info.id--;
-        PI.at(temp_info.id).velx = temp_info.velx;
-        PI.at(temp_info.id).vely = temp_info.vely;
-        PI.at(temp_info.id).velz = temp_info.velz;
+        pinfo.at(temp_info.id).velx = temp_info.velx;
+        pinfo.at(temp_info.id).vely = temp_info.vely;
+        pinfo.at(temp_info.id).velz = temp_info.velz;
     }
     ifs1.close();
     /*
@@ -138,26 +101,26 @@ int main(int argc, char *argv[])
         int i = temp_info.bond_pair[0] - 1;
         int j = temp_info.bond_pair[1] - 1;
         temp_info.bond_type[0]--;
-        PI.at(i).bond_pair[PI.at(i).nbond] = j;
-        PI.at(i).bond_type[PI.at(i).nbond] = temp_info.bond_type[0];
-        PI.at(i).nbond++;
+        pinfo.at(i).bond_pair[pinfo.at(i).nbond] = j;
+        pinfo.at(i).bond_type[pinfo.at(i).nbond] = temp_info.bond_type[0];
+        pinfo.at(i).nbond++;
 
-        PI.at(j).bond_pair[PI.at(j).nbond] = i;
-        PI.at(j).bond_type[PI.at(j).nbond] = temp_info.bond_type[0];
-        PI.at(j).nbond++;
+        pinfo.at(j).bond_pair[pinfo.at(j).nbond] = i;
+        pinfo.at(j).bond_type[pinfo.at(j).nbond] = temp_info.bond_type[0];
+        pinfo.at(j).nbond++;
     }
     ifs2.close();
     /*//bond情報をファイルに書き出す際に便利な書き方．
     for (int i = 0; i < 10; i++)
     {
-        //"&&"条件で，PI.at(i).nbond == 1のときに重複して出力してしまうのを防ぐ．
-        if (PI.at(i).nbond == 1 && PI.at(i).id < PI.at(i).bond_pair[0])
+        //"&&"条件で，pinfo.at(i).nbond == 1のときに重複して出力してしまうのを防ぐ．
+        if (pinfo.at(i).nbond == 1 && pinfo.at(i).id < pinfo.at(i).bond_pair[0])
         {
-            std::cout << PI.at(i).id + 1 << " " << PI.at(i).bond_pair[0] + 1 << " " << PI.at(i).bond_type[0] + 1 << std::endl;
+            std::cout << pinfo.at(i).id + 1 << " " << pinfo.at(i).bond_pair[0] + 1 << " " << pinfo.at(i).bond_type[0] + 1 << std::endl;
         }
-        else if (PI.at(i).nbond == 2)
+        else if (pinfo.at(i).nbond == 2)
         {
-            std::cout << PI.at(i).id + 1 << " " << PI.at(i).bond_pair[1] + 1 << " " << PI.at(i).bond_type[0] + 1 << std::endl;
+            std::cout << pinfo.at(i).id + 1 << " " << pinfo.at(i).bond_pair[1] + 1 << " " << pinfo.at(i).bond_type[0] + 1 << std::endl;
         }
     }*/
     /*
@@ -165,7 +128,7 @@ int main(int argc, char *argv[])
     
     
     
-    アングル情報の読み込みを行う．粒子番号や，アングル相手の粒子番号とアングル種が−1されていることに注意する．まだ途中*/
+    アングル情報の読み込みを行う．粒子番号や，アングル相手の粒子番号とアングル種が−1されていることに注意する．*/
     std::ifstream ifs3(argv[4]);
     if (!ifs3)
     {
@@ -179,32 +142,105 @@ int main(int argc, char *argv[])
         int j = temp_info.angle_pair[0][1] - 1;
         int k = temp_info.angle_pair[0][2] - 1;
         temp_info.angle_type[0]--;
-        PI.at(i).angle_pair[PI.at(i).nangle][0] = i;
-        PI.at(i).angle_pair[PI.at(i).nangle][1] = j;
-        PI.at(i).angle_pair[PI.at(i).nangle][2] = k;
-        PI.at(i).angle_type[PI.at(i).nangle] = temp_info.angle_type[0];
-        PI.at(i).nangle++;
+        pinfo.at(i).angle_pair[pinfo.at(i).nangle][0] = i;
+        pinfo.at(i).angle_pair[pinfo.at(i).nangle][1] = j;
+        pinfo.at(i).angle_pair[pinfo.at(i).nangle][2] = k;
+        pinfo.at(i).angle_type[pinfo.at(i).nangle] = temp_info.angle_type[0];
+        pinfo.at(i).nangle++;
 
-        PI.at(j).angle_pair[PI.at(j).nangle][0] = i;
-        PI.at(j).angle_pair[PI.at(j).nangle][1] = j;
-        PI.at(j).angle_pair[PI.at(j).nangle][2] = k;
-        PI.at(j).angle_type[PI.at(j).nangle] = temp_info.angle_type[0];
-        PI.at(j).nangle++;
+        pinfo.at(j).angle_pair[pinfo.at(j).nangle][0] = i;
+        pinfo.at(j).angle_pair[pinfo.at(j).nangle][1] = j;
+        pinfo.at(j).angle_pair[pinfo.at(j).nangle][2] = k;
+        pinfo.at(j).angle_type[pinfo.at(j).nangle] = temp_info.angle_type[0];
+        pinfo.at(j).nangle++;
 
-        PI.at(k).angle_pair[PI.at(k).nangle][0] = i;
-        PI.at(k).angle_pair[PI.at(k).nangle][1] = j;
-        PI.at(k).angle_pair[PI.at(k).nangle][2] = k;
-        PI.at(k).angle_type[PI.at(k).nangle] = temp_info.angle_type[0];
-        PI.at(k).nangle++;
+        pinfo.at(k).angle_pair[pinfo.at(k).nangle][0] = i;
+        pinfo.at(k).angle_pair[pinfo.at(k).nangle][1] = j;
+        pinfo.at(k).angle_pair[pinfo.at(k).nangle][2] = k;
+        pinfo.at(k).angle_type[pinfo.at(k).nangle] = temp_info.angle_type[0];
+        pinfo.at(k).nangle++;
     }
     ifs3.close();
     /*//アングル情報をファイルに書き出す際に便利な書き方．
     for (int i = 0; i < 10; i++)
     {
-        if (PI.at(i).nangle == 1)
+        if (pinfo.at(i).nangle == 1)
         {
-            std::cout << PI.at(i).angle_pair[0][0] + 1 << " " << PI.at(i).angle_pair[0][1] + 1 << " " << PI.at(i).angle_pair[0][2] + 1 << " " << PI.at(i).angle_type[0] + 1 << std::endl;
+            std::cout << pinfo.at(i).angle_pair[0][0] + 1 << " " << pinfo.at(i).angle_pair[0][1] + 1 << " " << pinfo.at(i).angle_pair[0][2] + 1 << " " << pinfo.at(i).angle_type[0] + 1 << std::endl;
         }
     }*/
+    /*
+    
+    
+    
+    
+    出力ファイルを生成する．*/
+    //open file
+    //pos_file
+    FILE *fpo0;
+    fpo0 = fopen("../output/initial_pos_lipid.cdv", "w");
+    if (fpo0 == NULL)
+    {
+        printf("ERROR_initial_pos_lipid.cdv\n");
+        return -1;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        fprintf(fpo0, "%s \n", delete_str[i].c_str());
+    }
+    for (int i = 0; i < pinfo.size(); i++)
+    {
+        fprintf(fpo0, "%d %d   %lf   %lf   %lf \n",
+                pinfo.at(i).id + 1,
+                pinfo.at(i).type + 1,
+                pinfo.at(i).posx,
+                pinfo.at(i).posy,
+                pinfo.at(i).posz);
+    }
+    /*
+
+
+
+
+
+    */
+    //vel_file
+    FILE *fpo1;
+    fpo1 = fopen("../output/initial_vel_lipid.cdv", "w");
+    if (fpo1 == NULL)
+    {
+        printf("ERROR_initial_vel_lipid.cdv\n");
+        return -1;
+    }
+    /*
+
+
+
+
+
+    */
+    //bond_file
+    FILE *fpo2;
+    fpo2 = fopen("../output/bond_info.cdv", "w");
+    if (fpo2 == NULL)
+    {
+        printf("ERROR_bond_info.cdv\n");
+        return -1;
+    }
+    /*
+
+
+
+
+
+    */
+    //angle_file
+    FILE *fpo3;
+    fpo3 = fopen("../output/angle_info.cdv", "w");
+    if (fpo3 == NULL)
+    {
+        printf("ERROR_angle_info.cdv\n");
+        return -1;
+    }
     return 0;
 }
